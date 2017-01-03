1cfe32ac7a5fcac3694a681f8e6cb64009d07cf1
MironAtHome <mironatpro@hotmail.com>
2017-01-03 12:52:59 -0800

    Math Fun initial check - in

---
+++ Multiplication/Multiplication.cs
@@ -0,0 +1,127 @@
+﻿namespace MathFun
+{
+    using System;
+    class Multiplication
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                float v1 = float.MaxValue;
+                float v2 = float.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2*v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                double v1 = double.MaxValue;
+                double v2 = double.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2*v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                uint v1 = uint.MaxValue;
+                uint v2 = uint.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2*v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                int v1 = int.MaxValue;
+                int v2 = int.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2*v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                short v1 = short.MaxValue;
+                short v2 = short.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (short)(v2*v1);
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                ushort v1 = ushort.MaxValue;
+                ushort v2 = ushort.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (ushort)(v2*v1);
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                long v1 = long.MaxValue;
+                long v2 = long.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (long)v2*v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                ulong v1 = ulong.MaxValue;
+                ulong v2 = ulong.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (ulong)v2*v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                byte v1 = byte.MaxValue;
+                byte v2 = byte.MaxValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (byte)(v2*v1);
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ Subtruction/Subtruction.cs
@@ -0,0 +1,127 @@
+﻿namespace MathFun
+{
+    using System;
+    class Subtruction
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                float v1 = float.MaxValue;
+                float v2 = float.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2-v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                double v1 = double.MaxValue;
+                double v2 = double.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2-v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                uint v1 = uint.MaxValue;
+                uint v2 = uint.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2-v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                int v1 = int.MaxValue;
+                int v2 = int.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = v2-v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                short v1 = short.MaxValue;
+                short v2 = short.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (short)(v2-v1);
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                ushort v1 = ushort.MaxValue;
+                ushort v2 = ushort.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (ushort)(v2-v1);
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                long v1 = long.MinValue;
+                long v2 = long.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (long)v2-v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                ulong v1 = ulong.MaxValue;
+                ulong v2 = ulong.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (ulong)v2-v1;
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+            try
+            {
+                byte v1 = byte.MaxValue;
+                byte v2 = byte.MinValue;
+                Console.WriteLine("Type: {0}", v1.GetType().Name);
+                Console.WriteLine("1. Value of v1 = {0}", v1);
+                v1 = (byte)(v2-v1);
+                Console.WriteLine("2. Value of v1 = {0}", v1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Message: {0}", ex.Message);
+            }
+        }
+    }
+}
